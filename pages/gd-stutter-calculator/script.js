85a61cf22035a44d4e76ed024d17b955c1ddaf86
Connor <ytthespectrewolf@gmail.com>
2025-11-13 16:45:37 -0600

    Update script.js
    Fixing things, not done yet

--- pages/gd-stutter-calculator/script.js
+++ pages/gd-stutter-calculator/script.js
@@ -1,13 +1,74 @@
 const pbInputs = document.querySelectorAll('input[name="pb"]');
 const pbEx = document.getElementById("pb-values");
+const frVal = document.getElementById("fr");
+const rrVal = document.getElementById("rr");
+const prVal = document.getElementById("pr");
+const startButton = document.getElementById("startB");
+let frRate;
+let rrRate;
+let prRate = 240;
 pbEx.style.display = "none";
 
-pbInputs.forEach(input => {
-  input.addEventListener('change', function() {
+console.log("Works");
+
+frVal.addEventListener("change", function(){
+  frRate = frVal.value;
+});
+
+rrVal.addEventListener("change", function(){
+  rrRate = rrVal.value;
+});
+
+prVal.addEventListener("change", function(){
+  prRate = rrVal.value;
+});
+
+
+
+pbInputs.forEach(element => {
+  element.addEventListener("change", function() {
     if(document.querySelector('input[name="pb"]:checked').value == "yes"){
       pbEx.style.display = "block";
-    else:
+    }
+    else{
       pbEx.style.display = "none";
     };
   });
 });
+
+function calcStutterVal(){
+  var endVal = (2*(prRate)*Math.PI)/prRate;
+  var frPoints = [];
+  var rrPoints = [];
+  var prPoints = [];
+  let prStutter = 0;
+  var rrStutter;
+  var leastVal = 10000000;
+  
+  for(let i = 1; i<=endVal; i++){
+    frPoints.push((2*Math.PI*i)/frRate);
+    rrPoints.push((2*Math.PI*i)/rrRate);
+    prPoints.push((2*Math.PI*i)/prRate);
+  }
+
+  prPoints.forEach(pt1 => 
+  {
+    frPoints.forEach(pt2 => {
+        
+
+      if(Math.abs(pt1 - pt2) < leastVal){
+          leastVal = Math.abs(pt1 - pt2);
+        }
+      else{
+          if(leastVal > (2*Math.PI)/prRate){
+            prStutter += 1;
+          }
+        }
+    })
+    console.log(prStutter);
+  });
+  
+  }
+  
+
+startButton.addEventListener("click", calcStutterVal);
